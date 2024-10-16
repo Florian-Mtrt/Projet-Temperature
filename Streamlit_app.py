afe160cbc0c08fbf85a2a6141c77024786e051b2
Florian Delattre <69481375+floriandelattre@users.noreply.github.com>
2024-10-16 21:13:44 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -56,7 +56,9 @@ if page == pages[0] :
   
   L’objectif de ce projet est de constater le réchauffement climatique et le dérèglement climatique global à l’échelle de la planète sur les derniers siècles et dernières décennies. Ce phénomène sera analysé au niveau mondial et par zone géographique. 
   \n
-  Le projet a été piloté par Philippe Grenesche, Yves Liais, Florian Matrat et moi-même Florian Delattre.
+  \n
+  Le projet a été piloté par Philippe Grenesche, Yves Liais, Florian Matrat et moi-même Florian Delattre
+  \n
   et a été supervisé par Alain Ferlac.
 
   """
