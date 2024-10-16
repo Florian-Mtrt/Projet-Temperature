f70c131a0d8339bbbf5fb3784f2f03085d4e5750
Yves-63 <yves.liais@michelin.com>
2024-10-16 17:23:31 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -812,8 +812,7 @@ if page == pages[3] :
   """
   st.write(texte_modelisation_y_2)
 
-  fig_y = go.Figure()
-  fig_y.add_trace(go.Scatter(df_y_pred, x="year", y="temperature_change", mode="lines", name="T°C réelle, monde", marker_color="red"))
+  fig_y = px.Scatter(df_y_pred, x="year", y="temperature_change", mode="lines", name="T°C réelle, monde", marker_color="red")
   fig_y.add_trace(go.Scatter(df_y_pred, x="year", y="prediction", mode="lines", name="prédiction, monde", marker_color="red"))                 
 
   st.plotly_chart(fig_y)
