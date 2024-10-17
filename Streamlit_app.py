64d9b4faad0e0095284f6b81aa913f67d9446374
Florian-Mtrt <matratflorian@gmail.com>
2024-10-17 18:44:50 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1132,7 +1132,7 @@ if page == pages[3] :
 
   plt.figure(figsize=(10, 6))
   pivot_table = heatmap_data_melted.pivot("Zone", "Year", "Anomalie")
-  sns.heatmap(pivot_table, cmap='coolwarm', annot=True, fmt=".2f", linewidths=.5)
+  sns.heatmap(pivot_table, cmap='coolwarm', annot=False, fmt=".2f", linewidths=.5)
   plt.title('Heatmap des anomalies de SST par zone')
   plt.xlabel('Année')
   plt.ylabel('Zones')
