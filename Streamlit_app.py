1099e3922f4af6186b0b4d6eafd269b736e60b61
Florian-Mtrt <matratflorian@gmail.com>
2024-10-17 19:11:44 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1125,25 +1125,27 @@ if page == pages[3] :
     )
   st.plotly_chart(fig)
 
-# ---- VISUALISATION DE LA HEATMAP (Nord, Équateur, Sud) ---- #
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 6))
+  # ---- VISUALISATION DE LA HEATMAP (Nord, Équateur, Sud) ---- #
+  fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 12))
 
-heatmap_data = Resultats_ZonAnn_Ts_dSST[['Year', '24N-90N', '24S-24N', '90S-24S']]
-heatmap_data_melted = heatmap_data.melt(id_vars='Year', var_name='Zone', value_name='Anomalie')
+  heatmap_data = Resultats_ZonAnn_Ts_dSST[['Year', '24N-90N', '24S-24N', '90S-24S']]
+  heatmap_data_melted = heatmap_data.melt(id_vars='Year', var_name='Zone', value_name='Anomalie')
 
-pivot_table = heatmap_data_melted.pivot_table(index="Zone", columns="Year", values="Anomalie", aggfunc='mean')
-sns.heatmap(pivot_table, cmap='RdYlBu_r', annot=False, fmt=".2f", linewidths=.5, ax=axes[0])
-axes[0].set_title('Heatmap des Températures Historiques et des Prédictions Futures (Nord, Équateur, Sud)')
-axes[0].set_xlabel('Année')
-axes[0].set_ylabel('Hémisphère')
+  pivot_table = heatmap_data_melted.pivot_table(index="Zone", columns="Year", values="Anomalie", aggfunc='mean')
+  sns.heatmap(pivot_table, cmap='RdYlBu_r', annot=False, fmt=".2f", linewidths=.5, ax=axes[0])
+  axes[0].set_title('Heatmap des Températures Historiques et des Prédictions Futures (Nord, Équateur, Sud)')
+  axes[0].set_xlabel('Année')
+  axes[0].set_ylabel('Hémisphère')
 
-# ---- VISUALISATION DE LA HEATMAP des Températures Historiques et des Prédictions Futures pour les Hémisphères ---- #
-heatmap_data_2 = Resultats_ZonAnn_Ts_dSST[['Year', '64N-90N', '44N-64N', '24N-44N', 'EQU-24N', '24S-EQU', '44S-24S', '64S-44S', '90S-64S']]
-heatmap_data_melted_2 = heatmap_data_2.melt(id_vars='Year', var_name='Zone', value_name='Anomalie')
+  # ---- VISUALISATION DE LA HEATMAP des Températures Historiques et des Prédictions Futures pour les Hémisphères ---- #
+  heatmap_data_2 = Resultats_ZonAnn_Ts_dSST[['Year', '64N-90N', '44N-64N', '24N-44N', 'EQU-24N', '24S-EQU', '44S-24S', '64S-44S', '90S-64S']]
+  heatmap_data_melted_2 = heatmap_data_2.melt(id_vars='Year', var_name='Zone', value_name='Anomalie')
 
-pivot_table_2 = heatmap_data_melted_2.pivot_table(index="Zone", columns="Year", values="Anomalie", aggfunc='mean')
-sns.heatmap(pivot_table_2, cmap='RdYlBu_r', annot=False, fmt=".2f", linewidths=.5, ax=axes[1])
-axes[1].set_title('Heatmap des Températures Historiques et des Prédictions Futures pour les Hémisphères')
-axes[1].set_xlabel('Année')
-axes[1].set_ylabel('Hémisphère')
-st.pyplot(fig)
+  pivot_table_2 = heatmap_data_melted_2.pivot_table(index="Zone", columns="Year", values="Anomalie", aggfunc='mean')
+  sns.heatmap(pivot_table_2, cmap='RdYlBu_r', annot=False, fmt=".2f", linewidths=.5, ax=axes[1])
+  axes[1].set_title('Heatmap des Températures Historiques et des Prédictions Futures pour les Hémisphères')
+  axes[1].set_xlabel('Année')
+  axes[1].set_ylabel('Hémisphère')
+
+  plt.tight_layout()
+  st.pyplot(fig)
