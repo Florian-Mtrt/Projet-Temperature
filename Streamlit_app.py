9d29a6cf4078e90b6e0a7b6cda98a42b4606af55
philippegre <p.grenesche@kp1.fr>
2024-10-17 13:16:48 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -271,9 +271,9 @@ if page == pages[2] :
 
   fig5 = px.scatter(df_season, x="Year", y="Value", color="Season",
                     trendline="ols", # ligne de lissage de nuage de points des moindres carrés
-                    trendline_color_override="red",
+                    trendline_color_override="black",
                     facet_col='Season',
-                    facet_col_wrap=2,
+                    facet_col_wrap=3,
                     labels={
                      "Year": "Année",
                      "Value": "Ecart de température",
@@ -288,9 +288,9 @@ if page == pages[2] :
 
   fig6 = px.scatter(df_season, x="Year", y="Value", color="Season",
                  trendline='lowess', # ligne de lissage de nuage de points localement pondérée
-                 trendline_color_override="red",
+                 trendline_color_override="black",
                  facet_col='Season',
-                 facet_col_wrap=2,
+                 facet_col_wrap=3,
                  labels={
                      "Year": "Année",
                      "Value": "Ecart de température",
